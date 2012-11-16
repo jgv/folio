e9a1d972a0a5654e13e3d0d76065eca89ebcb7be
Jonathan Vingiano <jvingiano@gmail.com>
2012-11-16 17:15:55 -0500

    refactor

--- index.php
+++ index.php
@@ -1,6 +1,1 @@
-<?php
-error_reporting(E_ALL);
-require_once(dirname(__FILE__) . "/src/folio.php");
-$folio = new Folio();
-$folio->load_app();
-?>
+<?php require_once(dirname(__FILE__) . "/lib/folio.php"); ?>
